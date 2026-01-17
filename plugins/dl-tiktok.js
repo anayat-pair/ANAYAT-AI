0525ff9e27e7b80563a3701477eb6852f7cc6fc9
ANAYAT-AI <anayatbot207@gmail.com>
2026-01-18 01:50:40 +0500

    ANAYAT-AI

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -16,7 +16,7 @@ async (conn, mek, m, { from, args, q, reply }) => {
         
         reply("Downloading video, please wait...");
         
-        const apiUrl = `https://delirius-apiofc.vercel.app/download/tiktok?url=${q}`;
+        const apiUrl = `https://arslanmd-api.vercel.app/api/ttdl?url=${q}`;
         const { data } = await axios.get(apiUrl);
         
         if (!data.status || !data.data) return reply("Failed to fetch TikTok video.");
